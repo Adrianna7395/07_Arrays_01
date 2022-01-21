411da73d0accc46935990b2cfd64913a781690f2
Adriana <hrl.davidson@gmail.com>
2022-01-21 10:57:54 +0100

    inkrement, dekrement

--- script_01.js
+++ script_01.js
@@ -16,8 +16,31 @@
 // output(arr.length); // Anzahl der Elemente
 // output(arr[arr.length -1]);     //Index (zeigt welche Position im Array)
 
+// Theorie: Schleifen (for-schleife)
+// For -Schleife als Wiederholungs-Struktur
 
 
+// Inkrement (untere --> obere Grenze)
+// for (let i = 10; i <= 20; i++) 
+// {
+//     output("index i: " + i);
+    
+// }
+
+// Dekrement ( obere --> untere Grenze)
+// for (let i = 10; i > 0; i--) 
+// {
+//     output("index i: " + i);
+    
+// }
+
+// Inkrement (var. Schrittweite)
+// for (let i = 0; i <= 100; i+=10) 
+// {
+//     output("index i: " + i);
+
+
+// }
 
 
 // Funktionalität mit Einzelparametern
@@ -43,8 +66,11 @@ function getSentence(word1,word2,word3,word4) {         //word sind Parametern
 
 
 // Funktionalität mit Array 1
+// word1, word2, wordN ist Semantische Struktur
+// arr[0],arr[1],arr[n] ist Numerische Struktur
+// Transformation semantisches in numerisches Problem
 
-output(getSentenceArr(["Ich","bin","Maxine","Mütze"]));
+// output(getSentenceArr(["Ich","bin","Maxine","Mütze"]));
 function getSentenceArr(arr) {         //word sind Parametern
 
     const gap = " ";
